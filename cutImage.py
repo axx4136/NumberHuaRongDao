26420f61931aa15605dfd4f5a09c9457e6ca7db6
axx4136 <70749495+axx4136@users.noreply.github.com>
2020-10-15 16:18:42 +0800

    将获取到的图片切成九个小图片并存入本地

---
+++ cutImage.py
@@ -0,0 +1,24 @@
+import os
+
+from PIL import Image
+import url as URL
+def cut_image(image):
+    width, height = image.size
+    item_width = int(width / 3)
+    box_list = []
+    # (left, upper, right, lower)
+    for i in range(0, 3):  # 两重循环，生成9张图片基于原图的位置
+        for j in range(0, 3):
+            # print((i*item_width,j*item_width,(i+1)*item_width,(j+1)*item_width))
+            box = (j * item_width, i * item_width, (j + 1) * item_width, (i + 1) * item_width)
+            box_list.append(box)
+
+    image_list = [image.crop(box) for box in box_list]
+    return image_list
+def save_images(image_list):
+    index = 1
+    for image in image_list:
+        image.save('./QuestionCut/'+str(index) + '.png')
+        index += 1
+img=Image.open('./question.png')
+save_images(cut_image(img))
